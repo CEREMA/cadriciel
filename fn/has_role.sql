4fc2bcdd353dd88e09af553a8e28faa265cd927e
Stéphane Zucatti <stephane.zucatti@gmail.com>
2025-04-22 10:51:17 +0200

    fix

--- fn/has_role.sql
+++ fn/has_role.sql
@@ -3,10 +3,11 @@ RETURNS BOOLEAN
 LANGUAGE sql
 AS $$
 SELECT EXISTS (
-    SELECT 1 
-    FROM auth.profil p 
+    SELECT 1
+    FROM auth.profil p
+    JOIN auth.users u ON p.utilisateur_id = u.id
     JOIN auth.roles r ON p.role_id = r.id
-    WHERE p.utilisateur_id = auth.uid()
-    AND r.name = ANY(role_names)
+    WHERE u.orion_id = auth.uid()
+      AND r.name = ANY(role_names)
 );
 $$;
