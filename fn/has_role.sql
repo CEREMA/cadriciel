7592622b8f0d5de028a8f6b1203fedeb818ffbb7
Stéphane Zucatti <stephane.zucatti@gmail.com>
2025-04-24 15:25:46 +0200

    updates

--- fn/has_role.sql
+++ fn/has_role.sql
@@ -1,13 +1,15 @@
-CREATE OR REPLACE FUNCTION auth.has_role(role_names TEXT[])
-RETURNS BOOLEAN
-LANGUAGE sql
-AS $$
-SELECT EXISTS (
-    SELECT 1
-    FROM auth.profil p
-    JOIN auth.users u ON p.utilisateur_id = u.id
-    JOIN auth.roles r ON p.role_id = r.id
-    WHERE u.orion_id = auth.uid()
-      AND r.name = ANY(role_names)
-);
-$$;
+  CREATE OR REPLACE FUNCTION auth.has_role(role_names TEXT[])
+  RETURNS BOOLEAN
+  LANGUAGE sql
+  AS $$
+  BEGIN
+    SELECT EXISTS (
+        SELECT 1
+        FROM auth.profil p
+        JOIN auth.users u ON p.utilisateur_id = u.id
+        JOIN auth.roles r ON p.role_id = r.id
+        WHERE u.orion_id = auth.uid()
+          AND r.name = ANY(role_names)
+    );
+  END;
+  $$;
